783cc969c76c62f8512f96f174db076e1cd9c094
carlockk <carlos.virtualdesk@gmail.com>
2025-07-16 12:44:03 -0400

    probando altura del sidebar

--- src/components/SidebarFiltros.jsx
+++ src/components/SidebarFiltros.jsx
@@ -126,7 +126,7 @@ const SidebarFiltros = ({ onFiltrar }) => {
         </div>
       )}
 
-      <div className="hidden md:block fixed right-0 top-[80px] bottom-[0] w-64 bg-white border-l border-gray-200 overflow-y-auto z-40">
+      <div className="hidden md:block sticky top-20 w-64 bg-white border-l border-gray-200 max-h-[calc(100vh-5rem)] overflow-y-auto">
         <div className="p-4 space-y-6">{renderSidebarContent()}</div>
       </div>
     </>
